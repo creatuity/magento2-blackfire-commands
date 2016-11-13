85aacf5a474907a97a322d30aecec1dfad872eae
Joshua Warren <josh@creatuity.com>
2016-11-12 19:21:29 -0600

    Updating write YAML command to write tests

--- Console/Command/WriteYamlCommand.php
+++ Console/Command/WriteYamlCommand.php
@@ -41,16 +41,57 @@ class WriteYamlCommand extends Command
     {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $categoryFactory = $objectManager->create('Magento\Catalog\Model\ResourceModel\Category\CollectionFactory');
+        $baseURL = $objectManager->get('Magento\Store\Model\StoreManagerInterface')
+            ->getStore(1)
+            ->getBaseUrl();
         $categories = $categoryFactory->create()
             ->addAttributeToSelect('*')
             ->addAttributeToFilter('is_active',1)
             ->setOrder('position', 'ASC');
-        $output->writeln('<info>scenarios:</info>');
+        $output->writeln('scenarios:');
         foreach ($categories as $category) :
-            $output->writeln("<info>\t" . $category->getName() . "Category Page:</info>");
-            $output->writeln("<info>\t\t- " . $category->getURL() . "</info>");
-            $output->writeln("<info></info>");
+            $categoryURL = $category->getURL();
+            $categoryURL = str_replace($baseURL, '/', $categoryURL);
+            $output->writeln("     " . trim($category->getName()) . " Category Page:");
+            $output->writeln("          - " . $categoryURL . "");
         endforeach;
-        $output->writeln('<info></info>');
+        $output->writeln('');
+        $output->writeln("tests:");
+        $output->writeln("     assertions: ");
+        $output->writeln("          -");
+        $output->writeln("                  label: null");
+        $output->writeln("                  expression: 'metrics.sql.queries.count <= 10'");
+        $output->writeln("     path: '/.*'");
+        $output->writeln("     methods:");
+        $output->writeln("          - ANY");
+        $output->writeln("     assertions: ");
+        $output->writeln("          -");
+        $output->writeln("                  label: null");
+        $output->writeln("                  expression: 'metrics.composer.autoload.find_file.count <= 50'");
+        $output->writeln("     path: '/.*'");
+        $output->writeln("     command: '.*'");
+        $output->writeln("     methods:");
+        $output->writeln("          - ANY");
+        $output->writeln("     assertions: ");
+        $output->writeln("          -");
+        $output->writeln("                  label: null");
+        $output->writeln("                  expression: 'metrics.magento2.ee.installed.count == 0 or metrics.magento2.all.cache.full_page.builtin.hit.count == 1'");
+        $output->writeln("     path: '/.*'");
+        $output->writeln("     command: '.*'");
+        $output->writeln("     methods:");
+        $output->writeln("          - ANY");
+        $output->writeln("     assertions: ");
+        $output->writeln("          -");
+        $output->writeln("                  label: null");
+        $output->writeln("                  expression: 'metrics.magento2.ee.installed.count == 0 or metrics.magento2.all.cache.full_page.builtin.hit.count == 1 or metrics.magento2.all.frontend.blocks.with_lifetime.count > 0 and metrics.magento2.all.frontend.blocks.load_cache.hit.count > 0'");
+        $output->writeln("     path: '/.*'");
+        $output->writeln("     command: '.*'");
+        $output->writeln("     methods:");
+        $output->writeln("          - ANY");
+        $output->writeln('     "Pages should not become slower":');
+        $output->writeln('          path: "/.*"');
+        $output->writeln('          assertions:');
+        $output->writeln('               - "percent(main.wall_time) < 10%"       # time does not increase by more than 10%');
+        $output->writeln('               - "diff(metrics.sql.queries.count) < 2" # less than 2 additional SQL statements');
     }
 }
